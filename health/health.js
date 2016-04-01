353f4df33a8499c556c2bb5be7a58a739daeb740
Franco Román Meola <franco.meola@outlook.com>
2016-04-01 01:38:10 -0300

    Se cambió de d3 a c3 para hacer más fácil los gráficos. Ahora se muestran también datos de pasos y pisos. Por ahora para probar se muestran los datos crudos de un mes. Falta agrupar la data por día/semana/mes antes de generar los csv.

--- health/health.js
+++ health/health.js
@@ -1,65 +1,158 @@
 /**
- * https://bl.ocks.org/mbostock/3883245
+ * http://bl.ocks.org/aaizemberg/03f518eee62b22d7f4c5
  */
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var formatDate = d3.time.format("%Y-%m-%d %H:%M:%S %Z");
-
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.value); });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.csv("Health.csv", type, function(error, data) {
-    if (error) throw error;
-
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.value; }));
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        // .style("text-anchor", "end")
-        .text("kms");
-
-    svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
+var distanceChart = c3.generate({
+    bindto: '#distanceChart',
+    data: {
+        url: 'data/distanceWalkingRunning.csv',
+        x: 'date',
+        y: 'value',
+        xFormat: '%Y-%m-%d %H:%M:%S %Z',
+        names: {
+            value: 'Kms de caminata y trote'
+        },
+        type: 'bar'
+    },
+    color: {
+        pattern: ['#377eb8']
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                culling: true,
+                format: '%d/%m/%Y %H:%M'
+            },
+            label: {
+                text: 'Fecha'
+            }
+        },
+        y: {
+            label: {
+                text: 'Kms de caminata y trote'
+            }
+        }
+    },
+    grid: {
+        x: {
+            show: false
+        },
+        y: {
+            show: true
+        }
+    },
+    zoom: {
+        enabled: true
+    },
+    subchart: {
+        show: true
+    }
 });
 
-function type(d) {
-    d.date = formatDate.parse(d.date);
-    d.value = +d.value;
-    return d;
-}
+var flightsClimbedChart = c3.generate({
+    bindto: '#flightsClimbedChart',
+    data: {
+        url: 'data/flightsClimbed.csv',
+        x: 'date',
+        y: 'value',
+        xFormat: '%Y-%m-%d %H:%M:%S %Z',
+        names: {
+            value: 'Pisos Subidos'
+        },
+        type: 'bar'
+    },
+    color: {
+        pattern: ['#4daf4a']
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                culling: true,
+                format: '%d/%m/%Y %H:%M'
+            },
+            label: {
+                text: 'Fecha'
+            }
+        },
+        y: {
+            tick: {
+                culling: true,
+                values: [0,1,2,3,4,5]
+            },
+            label: {
+                text: 'Pisos Subidos'
+            }
+        }
+    },
+    grid: {
+        x: {
+            show: false
+        },
+        y: {
+            show: true
+        }
+    },
+    zoom: {
+        enabled: true
+    },
+    subchart: {
+        show: false
+    },
+    size: {
+        height: 240,
+        width: 480
+    }
+});
+
+var stepCountChart = c3.generate({
+    bindto: '#stepCountChart',
+    data: {
+        url: 'data/stepCount.csv',
+        x: 'date',
+        y: 'value',
+        xFormat: '%Y-%m-%d %H:%M:%S %Z',
+        names: {
+            value: 'Pasos'
+        },
+        type: 'bar'
+    },
+    color: {
+        pattern: ['#e41a1c']
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                culling: true,
+                format: '%d/%m/%Y %H:%M'
+            },
+            label: {
+                text: 'Fecha'
+            }
+        },
+        y: {
+            label: {
+                text: 'Pasos'
+            }
+        }
+    },
+    grid: {
+        x: {
+            show: false
+        },
+        y: {
+            show: true
+        }
+    },
+    zoom: {
+        enabled: true
+    },
+    subchart: {
+        show: false
+    },
+    size: {
+        height: 240,
+        width: 480
+    }
+});
